24559c528e7b281153166764a790f8a859bfdde3
Charity Parks <tuppercharity@gmail.com>
2019-08-20 04:19:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,29 @@
 def create_an_empty_array
-  
+ [] 
 end
 
 def create_an_array
-  
+  kids = ["Haley", "Jake", "Brendan", "Brady"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  kids = ["Haley", "Jake", "Brendan", "Brady"]
+  kids.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   kids = ["Haley", "Jake", "Brendan", "Brady", "arrays!"]
+   kids.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+ kids = ["wow", "Haley", "Jake", "Brendan", "Brady", "arrays!"]
+arrays = kids.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  kids = ["wow", "Haley", "Jake", "Brendan", "Brady"] 
+  wow = kids.shift
 end
 
 def retrieve_element_from_index(array, index_number)
