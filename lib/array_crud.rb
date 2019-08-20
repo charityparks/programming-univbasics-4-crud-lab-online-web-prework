0be6692b00fc4b435e8a7a34707beb84cbbc632d
Charity Parks <tuppercharity@gmail.com>
2019-08-20 04:21:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
    kids = ["Haley", "Jake", "Brendan", "Brady"]
-   kids[]
+   kids[3]
 end
 
 def retrieve_first_element_from_array(array)
