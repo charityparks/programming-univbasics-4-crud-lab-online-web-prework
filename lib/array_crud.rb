d4547016856e9d9f0453e2e0b6678875615be12c
Charity Parks <tuppercharity@gmail.com>
2019-08-20 04:23:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-   
-   kids[3]
+    kids = ["wow", "Haley", "Jake", "Brendan", "Brady", "am"]
+   kids[4]
 end
 
 def retrieve_first_element_from_array(array)
